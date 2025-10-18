5f9d6445b1e4bc864daa259ebcb942bbc6770482
justn-hyeok <justn.hyeok@gmail.com>
2025-10-19 03:13:44 +0900

    feat: script.js에서 라우팅 추가

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello, world!");
+import { router, navigateTo, renderCurrentRoute } from './src/router/router.js';
+
+window.navigateTo = navigateTo; 
+window.renderCurrentRoute = renderCurrentRoute;
+
+document.addEventListener('DOMContentLoaded', () => {
+    const startButton = document.getElementById('startButton');
+    
+    if (startButton) {
+        startButton.addEventListener('click', () => {
+            navigateTo('/quiz'); 
+        });
+        
+    }
+    
+    window.addEventListener('popstate', router);
+
+    if (window.location.pathname !== '/') {
+        router();
+    }
+});
